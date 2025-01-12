89cd31aa6b21e464c4e7237263056e5ece1e8abe
pradipta610 <pradipta.wistika@gmail.com>
2025-01-12 15:23:21 +0700

    change link image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   const products = [
     {
       id: "1",
-      image: "https://via.placeholder.com/150",
+      image: "https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Kamera DSLR",
       rating: 4.5,
       ratingText: "Sangat Bagus",
@@ -15,7 +15,7 @@ export default function Home() {
     },
     {
       id: "2",
-      image: "https://via.placeholder.com/150",
+      image: "https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Drone",
       rating: 4.8,
       ratingText: "Terbaik",
@@ -25,7 +25,7 @@ export default function Home() {
     },
     {
       id: "3",
-      image: "https://via.placeholder.com/150",
+      image: "https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Laptop Gaming",
       rating: 4.3,
       ratingText: "Bagus",
@@ -35,7 +35,7 @@ export default function Home() {
     },
     {
       id: "4",
-      image: "https://via.placeholder.com/150",
+      image: "https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Tripod Kamera",
       rating: 4.7,
       ratingText: "Sangat Memuaskan",
@@ -45,7 +45,7 @@ export default function Home() {
     },
     {
       id: "5",
-      image: "https://via.placeholder.com/150",
+      image: "https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Lampu Studio",
       rating: 4.6,
       ratingText: "Cerah",
@@ -55,7 +55,7 @@ export default function Home() {
     },
     {
       id: "6",
-      image: "https://via.placeholder.com/150",
+      image: "https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Mikrofon Podcast",
       rating: 4.9,
       ratingText: "Kualitas Suara Bagus",
@@ -65,7 +65,7 @@ export default function Home() {
     },
     {
       id: "7",
-      image: "https://via.placeholder.com/150",
+      image: "https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Smartphone Gimbal",
       rating: 4.4,
       ratingText: "Stabil",
@@ -75,7 +75,7 @@ export default function Home() {
     },
     {
       id: "8",
-      image: "https://via.placeholder.com/150",
+      image: "https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Action Camera",
       rating: 4.7,
       ratingText: "Cocok untuk Outdoor",
@@ -85,7 +85,7 @@ export default function Home() {
     },
     {
       id: "9",
-      image: "https://via.placeholder.com/150",
+      image: "https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Monitor 4K",
       rating: 4.8,
       ratingText: "Super Jernih",
@@ -95,7 +95,7 @@ export default function Home() {
     },
     {
       id: "10",
-      image: "https://via.placeholder.com/150",
+      image: "https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Projector",
       rating: 4.5,
       ratingText: "Tajam",
@@ -105,7 +105,7 @@ export default function Home() {
     },
     {
       id: "11",
-      image: "https://via.placeholder.com/150",
+      image: "https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Printer Laser",
       rating: 4.2,
       ratingText: "Ekonomis",
@@ -115,7 +115,7 @@ export default function Home() {
     },
     {
       id: "12",
-      image: "https://via.placeholder.com/150",
+      image: "https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Tablet Desain",
       rating: 4.8,
       ratingText: "Sangat Responsif",
@@ -136,7 +136,7 @@ export default function Home() {
     },
     {
       id: "2",
-      image: "https://via.placeholder.com/150",
+      image: "https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Drone",
       price: "Rp 500.000",
       quantity: 15,
@@ -144,7 +144,7 @@ export default function Home() {
     },
     {
       id: "3",
-      image: "https://via.placeholder.com/150",
+      image: "https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Laptop Gaming",
       price: "Rp 1.500.000",
       quantity: 10,
@@ -152,7 +152,7 @@ export default function Home() {
     },
     {
       id: "4",
-      image: "https://via.placeholder.com/150",
+      image: "https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Tripod Kamera",
       price: "Rp 100.000",
       quantity: 25,
@@ -160,7 +160,7 @@ export default function Home() {
     },
     {
       id: "5",
-      image: "https://via.placeholder.com/150",
+      image: "https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Lampu Studio",
       price: "Rp 200.000",
       quantity: 18,
@@ -168,7 +168,7 @@ export default function Home() {
     },
     {
       id: "6",
-      image: "https://via.placeholder.com/150",
+      image: "https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Mikrofon Podcast",
       price: "Rp 300.000",
       quantity: 22,
@@ -176,7 +176,7 @@ export default function Home() {
     },
     {
       id: "7",
-      image: "https://via.placeholder.com/150",
+      image: "https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Smartphone Gimbal",
       price: "Rp 250.000",
       quantity: 12,
@@ -184,7 +184,7 @@ export default function Home() {
     },
     {
       id: "8",
-      image: "https://via.placeholder.com/150",
+      image: "https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Action Camera",
       price: "Rp 450.000",
       quantity: 19,
