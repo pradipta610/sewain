d6fcd856a107e18fefd8101759865cc28aa79b1d
pradipta610 <pradipta.wistika@gmail.com>
2025-10-09 11:40:47 +0800

    add latest data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Category } from "@/components/category/index";
+import { HeroSection } from "@/components/homepage/hero-section";
 import { Product } from "@/components/product/index";
 import { productsCategories } from "@/data/categoriesData";
 
@@ -6,6 +7,7 @@ export default function Home() {
 
   return (
     <>
+    <HeroSection/>
       <Category productsCategories={productsCategories} />
       <Product productsCategories={productsCategories} />
     </>
